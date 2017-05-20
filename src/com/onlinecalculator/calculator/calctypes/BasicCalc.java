bd12261800510e287f48e0fd1cc1f2ba3b1c0f52
iStar <iStar@192.168.100.4>
2017-05-20 17:40:47 +0530

    The basic calculator program successfully executed...

--- src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
@@ -1,49 +1,102 @@
 package com.onlinecalculator.calculator.calctypes;
 import java.util.Scanner;
 
-public class BasicCalc
+ class Calc1
 {
-	
-	public BasicCalc()
+	int a,b,c,ch,res;
+	public Calc1()
         {
-		
+		a=0;
+		b=0;
+		c=0;
+		res=0;
 	}
 	
 	public int menu()
         {
-    	
+		System.out.println("======select option===========");
+		System.out.println("1.addition");
+		System.out.println("2.substraction");
+		System.out.println("3.multiplication");
+		System.out.println("4.division");
+		System.out.println("Enter your choice: ");
+		Scanner s=new Scanner(System.in);
+		ch=s.nextInt();
+		return 0;
 	}
 	
     public void userInputs()
     {
-    	
+    	Scanner s=new Scanner(System.in);
+    	System.out.println("enter the first number");
+    	a=s.nextInt();
+    	System.out.println("enter the second number");
+    	b=s.nextInt();
     	
     }
 
     public void performOperation()
 {
-    	
+    	switch(ch)
+		{
+		case 1:System.out.println("add");
+		res=add();
+		System.out.println(res);
+		break;
+		case 2:System.out.println("substact");
+		res=subtract();
+		System.out.println(res);
+		break;
+		case 3:System.out.println("multiply");
+		res=multiply();
+		System.out.println(res);
+		break;
+		case 4:System.out.println("divide");
+		res=divide();
+		System.out.println(res);
+		break;
+		default:System.out.println("invalid choice");
+		
+		
         }
        
     }
     
 	int add()
 	{
-		
+	c=a+b;
+	return c;
 	}
 	
 	int subtract()
 	{
-		
+		c=a-b;
+		return c;	
 	}
 	
 	int multiply()
 	{
-		
+		c=a*b;
+		return c;
 	}
 	
 	int divide()
 	{
+		c=a/b;
+		return c;
 	}
 	
 }
+ public class BasicCalc
+ {
+	 public static void main(String args[])
+	 {
+	 Calc1 c= new Calc1();
+	 c.menu();
+	 c.userInputs();
+	 c.performOperation();
+	 
+	 }
+ }
+ 
+ 
