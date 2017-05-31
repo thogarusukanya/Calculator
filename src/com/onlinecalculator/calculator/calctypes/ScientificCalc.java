481dd3629d2325d0823e9d97eebbd7c2377069d9
HOME <HOME@192.168.0.2>
2017-05-31 11:32:46 +0530

    Scientific Calculator Program-T. Sukanya

--- src/com/onlinecalculator/calculator/calctypes/ScientificCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/ScientificCalc.java
@@ -1,40 +1,108 @@
 package com.onlinecalculator.calculator.calctypes;
-
 import java.util.Scanner;
-
+import java.lang.Character;
 import java.lang.Math;
 
-public class ScientificCalc extends BasicCalc{
+public class ScientificCalc extends BasicCalc3{
 	public int menu(){
+		
+				System.out.println("======select option===========");
+				System.out.println("1.addition");
+				System.out.println("2.substraction");
+				System.out.println("3.multiplication");
+				System.out.println("4.division");
+				System.out.println("5.powerof");
+				System.out.println("6.squareroot of");
+				System.out.println("7.sinefunctionof");
+				System.out.println("Enter your choice: ");
+				Scanner s=new Scanner(System.in);
+				ch=s.nextInt();
+				return ch;
   	
 	}
 
   public void userInputs()
   {
   	
-  }
-
-  public void performOperation(){
-  		switch(choice)
-  		{
+	  Scanner s=new Scanner(System.in);
+  	System.out.println("enter the first number");
+  	a=s.nextInt();
+  	if(ch!=6&&ch!=7){
   		
-  	}
-      
+  	System.out.println("enter the second number");
+  	b=s.nextInt();
   }
   
-	double pwrof()
-	{
+  }
+  public void performOperation(){
+ 
+  		switch(ch)
+		{
+		case 1:
 		
-	}
 	
-	double sqrtof()
-	{
-		
-	}
+		case 2:
+			
+		case 3:
+			
+		case 4:
+			
 
-	double sineof()
-	{
-		
-	}
+		super.performOperation();
+		break;
+		case 5:System.out.println(pwrof());
+				break;
+		case 6:System.out.println(sqrtof());
+				break;
+		case 7:System.out.println(sineof());
+				break;
+		default:System.out.println("wrong choice ");
+				break;
+		}
+  		System.out.println("\n=====\n");
+  }
+  double pwrof()
+  { 
+	  System.out.println("power");  
+	  return(Math.pow(a, b));
+	  
+  }
+  double sqrtof()
+  { 
+	  System.out.println("p");  
+	  return(Math.sqrt(a));
+  }
+  double sineof()
+  { 
+	  double rad=Math.toRadians(a);
+	  return(Math.sin(rad));
+	  
+  }
+  public static void main(String args[])
+	 {
+	
+
+int x;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter Either 1. Basic Calc or 2. Scientific Calc ");
+x=s.nextInt();
+
+if(x==1)
+{
+	BasicCalc3 calc= new BasicCalc3();
+
+calc.menu();
+calc.userInputs();
+calc.performOperation();
 }
+if(x==2)
+{
+	ScientificCalc scalc= new ScientificCalc();
 
+scalc.menu();
+scalc.userInputs();
+scalc.performOperation();
+}	
+}
+}  
+				
